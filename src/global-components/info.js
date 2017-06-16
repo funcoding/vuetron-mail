5fd1908efa1add699d7a5169c8f05a64ec43f27b
Vinay Kumar <vinaykumar1989@gmail.com>
2017-06-16 19:22:50 +0530

    Make use of ecma 6 template

--- src/global-components/info.js
+++ src/global-components/info.js
@@ -1,8 +1,9 @@
 let Vue = require('../../node_modules/vue/dist/vue.min');
-
-let template = '<div style="border-radius: 4px; margin:5px; color: #31708f;background-color: #d9edf7; border: 1px solid #bce8f1;text-align: center">' +
-    '<p>{{ message }}</p>' +
-    '</div>';
+let style = 'border-radius: 4px; margin:5px; color: #31708f;background-color: #d9edf7; border: 1px solid #bce8f1;text-align: center';
+let template = `
+    <div style="${style}">
+        <p>{{ message }}</p>
+    </div>`;
 
 Vue.component('info', {
    template: template,
